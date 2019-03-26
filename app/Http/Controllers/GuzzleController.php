054f0d255cbfda8a614398f334542ce897a73298
Diar Kourehpaz <99diagha@gmail.com>
2019-03-26 16:33:12 +0100

    API key is now in .env

--- app/Http/Controllers/GuzzleController.php
+++ app/Http/Controllers/GuzzleController.php
@@ -7,8 +7,10 @@ use GuzzleHttp\Client;
 class GuzzleController extends Controller
 {
     public function get($endpoint, $query) {
+        $api_key = env('API_KEY');
+
         $client = new Client();
-        $request = new \GuzzleHttp\Psr7\Request('GET', "https://api.themoviedb.org/3/movie/{$endpoint}?api_key=441ac5f375693fc6cf2d7c5a00560aa2&language=en-US&{$query}");
+        $request = new \GuzzleHttp\Psr7\Request('GET', "https://api.themoviedb.org/3/movie/{$endpoint}?api_key={$api_key}&language=en-US&{$query}");
         $promise = $client->sendAsync($request)->then(function ($response) {
             $data = json_decode($response->getBody(), true);
             return $data;
