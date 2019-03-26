5cacdf76e5b8f0040a6aa3cfd20bef68ba9f5367
Diar Kourehpaz <99diagha@gmail.com>
2019-03-26 17:21:01 +0100

    Can go to movie details route now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,11 @@
     <div class="row">
         @foreach ($movies as $movie)
             <div class="col-lg-2 mb-1">
-                <img class="poster" src="https://image.tmdb.org/t/p/w370_and_h556_bestv2/{{$movie['poster_path']}}" alt="{{$movie['title']}}">
+                <a href="{{ route('movie', ['id' => $movie['id']]) }}">
+                    <img class="poster"
+                         src="https://image.tmdb.org/t/p/w370_and_h556_bestv2/{{$movie['poster_path']}}"
+                         alt="{{$movie['title']}}">
+                </a>
             </div>
         @endforeach
     </div>
