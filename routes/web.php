66290fe63b3c66358ec4ae42cbe5f0db4b9a354c
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-03-27 17:21:33 +0100

    Add all routes needed to routes/web.php in a new routes branch that is up to date with master branch.

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,29 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/movie/{id}', function () {
-    dd('hi');
-})->name('movie');
+Route::get('/movies', 'MoviesController@searchResult')->name('movies');
+
+Route::get('/movie/{movie_id}', 'MovieController@movie')->name('movie');
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/user/{user_id}', 'UserController@user')
+->name('user');
+
+Route::get('/user/{user_id}/watchlists', 'UserController@watchlists')->name('watchlists');
+
+Route::get('/user/{user_id}/watchlists/{watchlist_id}', 'UserController@watchlistMovies')
+->name('watchlist');
+
+Route::get('/user/{user_id}/settings', 'UserController@settings')
+->name('settings');
+
+Route::get('/admin/{user_id}', 'AdminController@adminDashboard')->name('admin');
+
+Route::get('/admin/users', 'AdminController@users')->name('users');
+
+Route::get('/admin/reviews', 'AdminController@reviews')->name('reviews');
