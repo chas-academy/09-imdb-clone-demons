8f33f743f47b8279aea399d2b52aa89b4b616a7d
Diar Kourehpaz <99diagha@gmail.com>
2019-03-29 23:50:40 +0100

    Shows what movie you searched for now.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,25 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends GuzzleController
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-/*    public function __construct()
-    {
-        $this->middleware('auth');
-    }*/
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $data = parent::get('movie/popular', 'page=1');
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
-
-
 class SearchController extends GuzzleController
 {
     public function index() {
-        $query = Input::get('q');
+        $query = request()->query('q');
         $data = parent::get('search/movie', "query={$query}&page=1");
         return view('home', ['movies' => $data['results']]);
     }
